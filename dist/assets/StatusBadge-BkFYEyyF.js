6dadceb0dd1050660f3427744605a25cd23cab40
LayMatos <layssamatos8@gmail.com>
2025-03-31 23:48:53 -0300

    test

---
+++ dist/assets/StatusBadge-BkFYEyyF.js
@@ -0,0 +1,3 @@
+import{j as e}from"./index-DF4vcbd5.js";const a=({isDesaparecido:t})=>e.jsx("p",{className:`text-white text-lg font-bold px-4 py-2 rounded-lg w-fit shadow-md text-center
+      ${t?"bg-red-500":"bg-green-500"}
+    `,children:t?"Desaparecido":"Localizado"});export{a as S};
